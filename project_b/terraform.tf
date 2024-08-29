ccfdd1c6cec8ffe5cfbae3d59a4be1ea6ed4f0b0
Jayke Harrison <jayke.harrison@globallogic.com>
2024-08-29 16:06:03 +0100

    feat: add provider with tags for projects

---
+++ project_b/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.16.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+     Purpose = "tf-test"
+    }   
+  }
+}
